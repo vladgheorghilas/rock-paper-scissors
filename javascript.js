7bb5700c9b74985044266902afed58d091700284
vladgheorghilas <vlad.gheorghilas@gmail.com>
2022-11-24 12:36:13 +0200

    Add UI to game

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
-let n=parseInt(prompt("How many rounds do you want to play? No more than 7!"));
+/* let n=parseInt(prompt("How many rounds do you want to play? No more than 7!"));
 while(isNaN(n) || n>7)
 {
     if(isNaN(n))
         n=parseInt(prompt("Please introduce a number, not characters"));
     else
         n=parseInt(prompt("You can't play this game all day long! Introduce a number below 7."));
-}
+} */
 function getComputerChoice(){
     let choice=Math.floor(Math.random()*3);
     switch(choice){
@@ -17,26 +17,46 @@ function getComputerChoice(){
             return "Scissors";
     }
 }
-function playRound(playerSelection, computerSelection,i){
-    let playerChoice=playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    console.log(`Player chose ${playerChoice} and Computer chose ${computerSelection}`);
-    if(playerChoice==="Rock" && computerSelection==="Scissors" || 
-        playerChoice==="Paper" && computerSelection==="Rock" || 
-        playerChoice==="Scissors" && computerSelection==="Paper"){
-            console.log(`Round: ${i}: You win! ${playerChoice} beats ${computerSelection}`);
-            return 1;}
-    else if(playerChoice===computerSelection){
-        console.log(`Round: ${i}: It's a draw! You both chose ${playerChoice}`);
-        return 0;}
-    else{
-        console.log(`Round: ${i}: You lose! ${computerSelection} beats ${playerChoice}`);
-        return -1;}
-}
 let roundsWon=0;
 let roundsLost=0;
-let roundsDraw=0;
-let roundResult=0;
-function game(n){
+/* let roundsDraw=0; */
+/* let roundResult=0; */
+
+function playRound(e){
+    let playerChoice=e.target.textContent;
+    let computerSelection=getComputerChoice();
+    let roundRes=document.querySelector('.results');
+    roundRes.textContent=(`Player chose ${playerChoice} and Computer chose ${computerSelection}`);
+    if(playerChoice==="Rock" && computerSelection==="Scissors" || 
+        playerChoice==="Paper" && computerSelection==="Rock" || 
+        playerChoice==="Scissors" && computerSelection==="Paper")
+        {
+            roundRes.innerHTML+=`<br><br>You win! ${playerChoice} beats ${computerSelection}`;
+            roundsWon++;    
+        }
+    else if(playerChoice===computerSelection)
+        roundRes.innerHTML+=`<br><br>It's a draw! You both chose ${playerChoice}`;
+    else
+    {
+        roundRes.innerHTML+=`<br><br>You lose! ${computerSelection} beats ${playerChoice}`;
+        roundsLost++;
+    }
+    score.textContent=`${roundsWon} - ${roundsLost}`;
+    if(roundsWon===5){
+        score.innerHTML=`Player wins! <br> ${roundsWon} - ${roundsLost}`;
+        roundsWon=0;
+        roundsLost=0;
+    }
+    if(roundsLost===5){
+        score.innerHTML=`Computer wins! <br> ${roundsWon} - ${roundsLost}`;
+        roundsWon=0;
+        roundsLost=0;
+    }
+        
+}
+
+
+/* function game(n){
     for(i=1;i<=n;i++)
     {
         let playerSelection=prompt("Rock, Paper or Scissors?");
@@ -65,4 +85,17 @@ function game(n){
     else
         console.log(`You lost! ${roundsWon} rounds won, ${roundsDraw} draw rounds and ${roundsLost} lost rounds`);
 }
-game(n);
+game(n); */
+
+let rock=document.querySelector('.rock');
+let paper=document.querySelector('.paper');
+let scissors=document.querySelector('.scissors');
+let score=document.querySelector('.score');
+
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
+
+
+
